d04b399cde7e3dff9e03f95bcb5c100b3533f46f
yuli <yuli@xdja.com>
2021-01-29 14:57:32 +0800

    插槽的使用

---
+++ src/components/my-form.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Description: 
+ * @Author: yuli
+ * @Date: 2021-01-29 14:20:32
+ * @LastEditors: yuli
+ * @LastEditTime: 2021-01-29 14:50:11
+-->
+<template>
+  <view class="form">
+    <view class="form_title">标题</view>
+    <view class="form_content">
+      <slot></slot>
+    </view>
+  </view>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss"></style>
---
+++ src/pages/index6/index.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Description: 组件的使用、父子组件的通信
+ * @Author: yuli
+ * @Date: 2021-01-28 15:55:35
+ * @LastEditors: yuli
+ * @LastEditTime: 2021-01-29 14:51:24
+-->
+<template>
+  <view class="content">
+    <my-form>
+      <view>
+        <input type="text" />
+        <radio></radio>
+        <checkbox></checkbox>
+      </view>
+    </my-form>
+  </view>
+</template>
+
+<script>
+import myForm from "@/components/my-form"
+export default {
+  components: {
+    myForm,
+  },
+}
+</script>
+
+<style lang="scss"></style>
